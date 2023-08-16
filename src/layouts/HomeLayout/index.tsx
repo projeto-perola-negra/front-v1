606dea34eb88003d3fefc864375a4d97527894ea
Filype <lyperodrigues03@gmail.com>
2023-08-16 20:41:22 -0300

    fix: adjusts on layout

--- src/layouts/HomeLayout/index.tsx
+++ src/layouts/HomeLayout/index.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
+import Carousel from "../../components/Carousel";
 
 export interface HomeLayoutProps {
   children?: ReactNode;
@@ -12,6 +13,8 @@ export default function HomeLayout(props: HomeLayoutProps) {
       <header>
         <NavBar />
       </header>
+
+      <Carousel />
 
       <main className="py-16 flex flex-col gap-y-32 max-w-screen mx-auto">
         {props.children}
