d37f4d9960f5ef21355ef39c46006e36d916fb9a
Filype <lyperodrigues03@gmail.com>
2023-08-16 20:45:19 -0300

    fix: layout padding & bg

--- src/layouts/HomeLayout/index.tsx
+++ src/layouts/HomeLayout/index.tsx
@@ -9,20 +9,20 @@ export interface HomeLayoutProps {
 
 export default function HomeLayout(props: HomeLayoutProps) {
   return (
-    <div className="p-4">
+    <div className="bg-white">
       <header>
         <NavBar />
       </header>
 
-      <Carousel />
-
-      <main className="py-16 flex flex-col gap-y-32 max-w-screen mx-auto">
-        {props.children}
-      </main>
-
-      <footer>
-        <Footer />
-      </footer>
+      <div className="p-4">
+        <Carousel />
+        <main className="py-16 flex flex-col gap-y-32 max-w-screen mx-auto">
+          {props.children}
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
     </div>
   );
 }
