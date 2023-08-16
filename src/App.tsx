0cef67f7ce8984ac2ebc932771c564e42b909f21
Filype <lyperodrigues03@gmail.com>
2023-08-16 20:45:35 -0300

    fix: pass home page on App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-import Heading from "./components/Heading";
-import Text from "./components/Text";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return (
-    <>
-      <Heading variant="h1">Heading 1</Heading>
-      <Heading variant="h2">Heading 2</Heading>
-      <Heading variant="h3">Heading 3</Heading>
-
-      <Text size="paragraph">Paragraph</Text>
-      <Text size="small">Small</Text>
-      <Text size="xs">Extra Small</Text>
-      <Text size="2xs">2 Extra Small</Text>
-    </>
-  );
+  return <HomePage />;
 }
 
 export default App;
